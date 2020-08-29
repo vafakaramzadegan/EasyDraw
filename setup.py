190f7215682c692d1580fbf47583e500ca0f1bc8
Vafa Karamzadegan <63124707+vafakaramzadegan@users.noreply.github.com>
2020-08-29 10:17:51 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EasyDraw",
+    version="0.2.0",
+    author="Vafa Karamzadegan",
+    author_email="vafa.k@live.com",
+    description="A graphical library build for visual arts. EasyDraw is built on top of tkinter and has more functionalities.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vafakaramzadegan/EasyDraw",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
