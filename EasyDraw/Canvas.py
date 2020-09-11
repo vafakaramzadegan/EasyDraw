7863a779c824ebd91c3e54ca44584bf46735ee6b
Vafa Karamzadegan <63124707+vafakaramzadegan@users.noreply.github.com>
2020-09-11 00:49:05 -0700

    Add files via upload

--- EasyDraw/Canvas.py
+++ EasyDraw/Canvas.py
@@ -11,29 +11,31 @@ from EasyDraw import Vector
 class Canvas:
     '''Extends functionality of tk.canvas'''
     def __init__(self, master, **kwargs):
+        # stores pushed parameters
+        self.__hist = []
+        self.__hist.append({})
         # frames to export as a GIF file
         self.__frames = []
         # canvas origin position
-        self.__center_x        = 0
-        self.__center_y        = 0
+        self.__parameters()['center_x']        = 0
+        self.__parameters()['center_y']        = 0
         # rotation degree
-        self.__rotate_deg      = 0
+        self.__parameters()['rotate_deg']      = 0
         # fill and stroke color
-        self.__fill_color      = 'white'
-        self.__stroke_color    = 'black'
+        self.__parameters()['fill_color']      = 'white'
+        self.__parameters()['stroke_color']    = 'black'
         # stroke thickness
-        self.__stroke_width    = 1
-        self.__stroke_disabled = False
+        self.__parameters()['stroke_width']    = 1
+        self.__parameters()['stroke_disabled'] = False
         # vertices data to create polygon
         self.__vertices        = []
-
         self.__width           = kwargs.get('width', 0)
         self.__height          = kwargs.get('height', 0)
         # list of shapes drawn on PhotoImage
         self.__alpha_shapes    = []
         # font styles
-        self.__font_family     = 'Tahoma 20'
-        self.__font_color      = 'black'
+        self.__parameters()['font_family']     = 'Tahoma 20'
+        self.__parameters()['font_color']      = 'black'
 
         # tkinter's default methods are available in handle
         self.handle = tk.Canvas(master,
@@ -41,6 +43,8 @@ class Canvas:
                                 height             = self.__height,
                                 background         = kwargs.get('background', 'black'),
                                 highlightthickness = 0)
+        self.app = kwargs.get('app', None)
+        self.__showGrid = kwargs.get('showGrid', False)
         # no transparent background
         self.handle.create_rectangle(0, 0,
                                      self.__width, self.__height,
@@ -67,97 +71,118 @@ class Canvas:
         im = self.__pack()
         if im: self.__frames.append(im)
 
+    # save frames to file
     def save_frames(self, path, interval):
         try:
             self.__frames[0].save(path,
                                   format = 'GIF',
                                   append_images = self.__frames[1:],
                                   save_all = True,
+                                  optimize = True,
                                   duration = interval,
-                                  loop = 1)
+                                  loop = 0)
             return True
         except:
             raise Exception('ExportError: unable to save frames to GIF file!')
 
     # transform coordinates based on origin and rotation value
-    def __transform_coords(self, coords, offset=None):
+    def transform_coords(self, coords, offset=None):
         if offset is not None:
             cx = offset[0]
             cy = offset[1]
         else:
-            cx = self.__center_x
-            cy = self.__center_y          
-        angle = math.radians(self.__rotate_deg)
+            cx = self.__parameters()['center_x']
+            cy = self.__parameters()['center_y']
+
+        if self.app.useBounds:
+            cx = self.app.bound_center[0] + (cx * self.app.scale_x)
+            cy = self.app.bound_center[1] + (cy * self.app.scale_y)
+
+        angle = math.radians(self.__parameters()['rotate_deg'])
         cos_val = math.cos(angle)
         sin_val = math.sin(angle)
         new_coords = []
         for x_old, y_old in coords:
-            x_new = math.floor(x_old * cos_val - y_old * sin_val)
-            y_new = math.floor(x_old * sin_val + y_old * cos_val)
+            x_new = (x_old * cos_val - y_old * sin_val)
+            y_new = (x_old * sin_val + y_old * cos_val)
+
+            if self.app.useBounds:
+                x_new *= self.app.scale_x
+                y_new *= -self.app.scale_y
+
             new_coords.append((x_new + cx, y_new + cy))
+
         return new_coords
 
     # get center position
     def get_center_pos(self):
-        return (self.__center_x, self.__center_y)
+        return (self.__parameters()['center_x'], self.__parameters()['center_y'])
 
     # set the coordinates of canvas origin
     # all objects on the canvas are placed relative to these
     # values.
     def translate(self, x, y):
-        self.__center_x = math.floor(x)
-        self.__center_y = math.floor(y)
+        self.__parameters()['center_x'] = x
+        self.__parameters()['center_y'] = y
 
     # set rotation value
     def rotate(self, deg):
-        self.__rotate_deg = deg
+        self.__parameters()['rotate_deg'] = deg
 
     # set fill color
     def fill(self, color):
-        self.__fill_color = color
+        self.__parameters()['fill_color'] = color
 
+    # empty fill value
     def no_fill(self):
-        self.__fill_color = ''
+        self.__parameters()['fill_color'] = ''
 
     # set stroke color
     def stroke(self, color):
-        self.__stroke_color = color
-        self.__stroke_disabled = False
+        self.__parameters()['stroke_color'] = color
+        self.__parameters()['stroke_disabled'] = False
 
+    # disable stroke
     def no_stroke(self):
-        self.__stroke_disabled = True
+        self.__parameters()['stroke_disabled'] = True
 
+    # set stroke width
     def stroke_width(self, width):
-        self.__stroke_width = width
+        self.__parameters()['stroke_width'] = width
 
     # create circle
     def circle(self, x, y, radius, **kwargs):
-        points = self.__transform_coords([[x - radius, y - radius],
+        points = self.transform_coords([[x - radius, y - radius],
                                           [x + radius, y + radius]])
-        x1, y1 = points[0]
-        x2, y2 = points[1]
+        x1, y1 = (math.floor(points[0][0]), math.floor(points[0][1]))
+        x2, y2 = (math.floor(points[1][0]), math.floor(points[1][1]))
         cx = (x1 + x2) // 2
         cy = (y1 + y2) // 2
 
-        size = ((radius * 2) + 1, (radius * 2) + 1)
+        if self.app.useBounds:
+            size = ((radius * 2 * self.app.scale_x), (radius * 2 * self.app.scale_y))
+        else:
+            size = ((radius * 2), (radius * 2))
+
         alpha = int(kwargs.get('alpha', 1) * 255)
         bgAlpha = alpha
 
-        if (self.__fill_color == ''):
-            self.__fill_color = '#000'
+        if (self.__parameters()['fill_color'] == ''):
+            self.__parameters()['fill_color'] = '#000'
             bgAlpha = 0
 
-        fill = ImageColor.getrgb(self.__fill_color) + (bgAlpha,)
+        fill = ImageColor.getrgb(self.__parameters()['fill_color']) + (bgAlpha,)
 
-        if self.__stroke_disabled: self.__stroke_color = self.__fill_color
+        if self.__parameters()['stroke_disabled']:
+            self.__parameters()['stroke_color'] = self.__parameters()['fill_color']
 
-        stroke = ImageColor.getrgb(self.__stroke_color) + (alpha,)
+        stroke = ImageColor.getrgb(self.__parameters()['stroke_color']) + (alpha,)
         image = Image.new('RGBA', size)
 
         draw = ImageDraw.Draw(image)
         draw.ellipse((0, 0, size), fill = fill, outline = stroke)
 
-        self.__alpha_shapes.append(ImageTk.PhotoImage(image.rotate(-self.__rotate_deg, expand = True)))
+        self.__alpha_shapes.append(ImageTk.PhotoImage(image.rotate(-self.__parameters()['rotate_deg'], expand = True)))
             
         return self.handle.create_image(cx, cy,
                                         image=self.__alpha_shapes[-1],
@@ -165,43 +190,54 @@ class Canvas:
 
     # create rectangle
     def rect(self, x1, y1, x2, y2, **kwargs):
-        dx = x2 - x1
-        dy = y2 - y1
+        points = self.transform_coords([[x1, y1], [x2, y2]])
+        dx = (x2 - x1)
+        dy = (y2 - y1)
+      
+        if self.app.useBounds:
+            dx *= self.app.scale_x
+            dy *= self.app.scale_y
+
+        cx = (points[0][0] + points[1][0]) // 2
+        cy = (points[0][1] + points[1][1]) // 2
+
         alpha = int(kwargs.get('alpha', 1) * 255)
         bgAlpha = alpha
 
-        if (self.__fill_color == ''):
-            self.__fill_color = '#000'
+        if (self.__parameters()['fill_color'] == ''):
+            self.__parameters()['fill_color'] = '#000'
             bgAlpha = 0
 
-        if self.__stroke_disabled: self.__stroke_color = self.__fill_color
+        if self.__parameters()['stroke_disabled']:
+            self.__parameters()['stroke_color'] = self.__parameters()['fill_color']
 
-        fill = ImageColor.getrgb(self.__fill_color) + (bgAlpha,)
-        stroke = ImageColor.getrgb(self.__stroke_color) + (alpha,)
-        image = Image.new('RGBA', (dx, dy))
+        fill = ImageColor.getrgb(self.__parameters()['fill_color']) + (bgAlpha,)
+        stroke = ImageColor.getrgb(self.__parameters()['stroke_color']) + (alpha,)
+        image = Image.new('RGBA', (math.floor(dx), math.floor(dy)))
         draw = ImageDraw.Draw(image)
-        draw.rectangle((0, 0, dx, dy), fill = fill, outline = stroke, width = self.__stroke_width)
-        self.__alpha_shapes.append(ImageTk.PhotoImage(image.rotate(-self.__rotate_deg, expand = True)))
-        return self.handle.create_image(self.__transform_coords([[x1 + (dx / 2), y1 + (dy / 2)]]),
-                                        image=self.__alpha_shapes[-1],
+        draw.rectangle((0, 0, dx, dy), fill = fill, outline = stroke, width = self.__parameters()['stroke_width'])
+        self.__alpha_shapes.append(ImageTk.PhotoImage(image.rotate(self.__parameters()['rotate_deg'], expand = True)))
+        return self.handle.create_image(cx, cy,
+                                        image = self.__alpha_shapes[-1],
                                         anchor = 'center')
 
     # create line
-    # this method accepts two types of input
-    # the x and y values of start and end positions
-    # and also two vectors
-    @dispatch(int, int, int, int)
+    # this method accepts two types of .
+    # 1) starting and ending X & Ys
+    # 2) two vectors
+    @dispatch(object, object, object, object)
     def line(self, x1, y1, x2, y2):
-        return self.handle.create_line(self.__transform_coords([
+        return self.handle.create_line(self.transform_coords([
                                         [x1, y1],
                                         [x2, y2]]),
-                                        fill = self.__stroke_color,
-                                        width = self.__stroke_width)
+                                        fill = self.__parameters()['stroke_color'],
+                                        width = self.__parameters()['stroke_width'])
     @dispatch(Vector.Vector, Vector.Vector)
     def line(self, v1, v2):
         return self.line(int(v1.a), int(v1.b), int(v2.a), int(v2.b))
 
     # draw triangle on canvas using customShape
+    @dispatch(object, object, object, object, object, object)
     def triangle(self, x1, y1, x2, y2, x3, y3):
         tmp = self.__vertices
         self.begin_shape()
@@ -211,15 +247,20 @@ class Canvas:
         obj = self.end_shape()
         self.__vertices = tmp
         return obj
+    @dispatch(Vector.Vector, Vector.Vector, Vector.Vector)
+    def triangle(self, v1, v2, v3):
+        return self.triangle(v1.a, v1.b,
+                             v2.a, v2.b,
+                             v3.a, v3.b)
 
     # draw arc on canvas
     def arc(self, x1, y1, x2, y2, start, extend):
-        return self.handle.create_arc(self.__transform_coords([[x1, y1], [x2, y2]]),
+        return self.handle.create_arc(self.transform_coords([[x1, y1], [x2, y2]]),
                                       extent = -extend,
                                       start = -start,
                                       style = tk.ARC,
-                                      outline = self.__stroke_color,
-                                      width = self.__stroke_width)
+                                      outline = self.__parameters()['stroke_color'],
+                                      width = self.__parameters()['stroke_width'])
 
     '''create polygons---------------------------------------------------------------'''
     def begin_shape(self):
@@ -233,63 +274,77 @@ class Canvas:
             # get polygon bounding box
             xs = [x[0] for x in self.__vertices]
             ys = [y[1] for y in self.__vertices]
-            w = max(xs) * 2
-            h = max(ys) * 2
-            # set width and height of polygon
+            w = max(xs) * 3
+            h = max(ys) * 3
+
+            cx = self.__parameters()['center_x']
+            cy = self.__parameters()['center_y']
+            
+            if self.app.useBounds:
+                w = self.__width
+                h = self.__height
+                cx = self.app.bound_center[0]
+                cy = self.app.bound_center[1]
+
             size = (max(w, h), max(w, h))
+            offset = None if self.app.useBounds else (size[0] // 2, size[1] // 2)
+
+            # set width and height of polygon
             alpha = int(kwargs.get('alpha', 1) * 255)
             bgAlpha = alpha
 
-            if (self.__fill_color == ''):
-                self.__fill_color = '#000'
+            if (self.__parameters()['fill_color'] == ''):
+                self.__parameters()['fill_color'] = '#000'
                 bgAlpha = 0
 
-            fill = ImageColor.getrgb(self.__fill_color) + (bgAlpha,)
+            fill = ImageColor.getrgb(self.__parameters()['fill_color']) + (bgAlpha,)
 
-            if self.__stroke_disabled: self.__stroke_color = self.__fill_color
+            if self.__parameters()['stroke_disabled']:
+                self.__parameters()['stroke_color'] = self.__parameters()['fill_color']
             
-            stroke = ImageColor.getrgb(self.__stroke_color) + (alpha,)
+            stroke = ImageColor.getrgb(self.__parameters()['stroke_color']) + (alpha,)
             
             image = Image.new('RGBA', size)
             draw = ImageDraw.Draw(image)
 
-            draw.polygon(self.__transform_coords(self.__vertices, (size[0] // 2, size[1] // 2)),
+            draw.polygon(self.transform_coords(self.__vertices, offset),
                         fill = fill,
                         outline = stroke)
 
             self.__alpha_shapes.append(ImageTk.PhotoImage(image))
 
-            return self.handle.create_image(self.__center_x, self.__center_y,
-                                            image=self.__alpha_shapes[-1],
+            return self.handle.create_image(cx, cy,
+                                            image = self.__alpha_shapes[-1],
                                             anchor = 'center')
     '''------------------------------------------------------------------------------'''
 
     def font_family(self, family):
-        self.__font_family = family
+        self.__parameters()['font_family'] = family
 
     def font_color(self, color):
-        self.__font_color = color
+        self.__parameters()['font_color'] = color
 
     # put text on canvas
     def text(self, x, y, text):
-        return self.handle.create_text(self.__transform_coords([[x, y]]),
-                                       fill = self.__font_color,
-                                       font = self.__font_family,
+        return self.handle.create_text(self.transform_coords([[x, y]]),
+                                       fill = self.__parameters()['font_color'],
+                                       font = self.__parameters()['font_family'],
+                                       angle = -self.__parameters()['rotate_deg'],
                                        text = text)
 
     # set a pixel
     def point(self, x, y, color):
-        for point_x, point_y in self.__transform_coords([[x, y]]):
+        for point_x, point_y in self.transform_coords([[x, y]]):
             if point_x >= 0 and point_y >= 0:
-                return self.handle.create_line(self.__transform_coords([
+                return self.handle.create_line(self.transform_coords([
                                         [x, y],
                                         [x + 1, y + 1]]),
                                         fill = color)
                                         
     # get RGB value of a pixel
     def get_pixel(self, x, y):
-        x += self.__center_x
-        y += self.__center_y
+        x += self.__parameters()['center_x']
+        y += self.__parameters()['center_y']
         try:
             im = self.__pack()
             rgb_im = im.convert('RGB')
@@ -298,7 +353,57 @@ class Canvas:
         except:
             return (0, 0, 0)
 
+    def create_image(self, x, y, path, **kwargs):
+        im = Image.open(path)
+
+        scale = kwargs.get('scale', None)
+        if scale != None:
+            width, height = im.size
+            newWidth = math.floor(width * scale)
+            newheight = math.floor(height * scale)
+            im = im.resize((newWidth, newheight), Image.ANTIALIAS)
+
+        self.__alpha_shapes.append(ImageTk.PhotoImage(im.rotate(self.__parameters()['rotate_deg'], expand = True)))
+        return self.handle.create_image(self.transform_coords([[x, y]]),
+                                        image = self.__alpha_shapes[-1],
+                                        anchor = 'center')
+
+    # return the latest parameters
+    def __parameters(self):
+        return self.__hist[len(self.__hist) - 1]
+
+    # push current parameters
+    def push(self):
+        self.__hist.append(self.__parameters().copy())
+
+    # pop last parameters
+    def pop(self):
+        if len(self.__hist) == 1:
+            raise Exception('Cannot go back any furthur. List is empty!')
+        self.__hist.pop(len(self.__hist) - 1)
+
+    # draw grid on screen.
+    # requires boundaries to be set first
+    def showGrid(self):
+        if type(self.app.bounds) is tuple:
+            self.push()
+            self.stroke('#333')
+            self.stroke_width(1)
+            self.handle.create_rectangle(0, 0,
+                                         self.__width, self.__height,
+                                         fill = self.app.bg_color,
+                                         outline = self.app.bg_color)
+            
+            for i in range(self.app.bounds[0], self.app.bounds[2] + 1):
+                self.line(i, self.app.bounds[1], i, self.app.bounds[3])
+                
+            for i in range(self.app.bounds[1], self.app.bounds[3] + 1):
+                self.line(self.app.bounds[0], i, self.app.bounds[2], i)
+
+            self.pop()
 
     # clear canvas
     def clear(self, target):
         self.handle.delete(target)
+        if self.__showGrid:
+            self.showGrid()
--- EasyDraw/__init__.py
+++ EasyDraw/__init__.py
@@ -1,3 +1,13 @@
+'''
+    EasyDraw
+    -------------------------------
+    A graphical library built for visual arts.
+    EasyDraw is built on top of tkinter and has more functionalities.
+
+    Author: Vafa Karamzadegan
+    https://github.com/vafakaramzadegan/EasyDraw
+'''
+
 import tkinter as tk
 from EasyDraw import Color
 from EasyDraw import Canvas
@@ -6,51 +16,160 @@ from EasyDraw import Tools
 class EasyDraw(object):
     '''EasyDraw main class'''
     def __init__(self, **kwargs):
-        self.width       = kwargs.get('width', 400)
-        self.height      = kwargs.get('height', 400)
-        self.autoClear   = kwargs.get('autoClear', True)  
-        self.interval    = kwargs.get('fps', 30)
-        self.export_path = kwargs.get('exportPath', '')
-        self.mouse_x = 0
-        self.mouse_y = 0
-        self.mouse_left = 0
-        self.mouse_top = 0
+        print('Hello from EasyDraw!')
+        self.width        = kwargs.get('width', 400)
+        self.height       = kwargs.get('height', 400)
+        self.autoClear    = kwargs.get('autoClear', True)  
+        self.interval     = kwargs.get('fps', 30)
+        self.export_path  = kwargs.get('exportPath', '')
+        if self.export_path != '':
+            print('Recording frames...')
+        self.mouse_x      = 0
+        self.mouse_y      = 0
+        self.mouse_left   = 0
+        self.mouse_top    = 0
+
+        self.useBounds    = False
+        self.bounds       = (-5, -5, 5, 5)
+        self.scale_x      = 0
+        self.scale_y      = 0
+        self.bound_center = (0, 0)
+
         if self.interval not in range(0, 1001):
             raise ValueError("invalid fps value should be between 1 and 1000 but '%d' was entered." % self.interval)
         self.interval  = 1000 // self.interval
         
         master         = tk.Tk()
         self.master    = master
-        master.bind('<Motion>', self.__motion_event)
+
+        # bind mouse event handlers
+        master.bind('<Motion>'         , self.__motion_event)
+        master.bind('<Button-1>'       , self.__mouse_left_btn_click)
+        master.bind('<Button-2>'       , self.__mouse_middle_btn_click)
+        master.bind('<Button-3>'       , self.__mouse_right_btn_click)
+        master.bind('<B1-Motion>'      , self.__left_mouse_btn_down)
+        master.bind('<B3-Motion>'      , self.__right_mouse_btn_down)
+        master.bind('<ButtonRelease-1>', self.__left_mouse_btn_up)
+        master.bind('<ButtonRelease-3>', self.__right_mouse_btn_up)
+        
         master.title(kwargs.get('title', 'EasyDraw App'))
         master.protocol("WM_DELETE_WINDOW", self.__on_closing)
 
         self.bg_color = kwargs.get('background', 'silver')
         self.canvas = Canvas.Canvas(master,
+                            app = self,
                             width = self.width,
                             height = self.height,
-                            background = self.bg_color)
+                            background = self.bg_color,
+                            showGrid = kwargs.get('showGrid', False))
 
         self.tools = Tools.Tools()
         self.color = Color.Color()
 
         self.setupFunction  = kwargs.get('setupFunc', None)
         self.drawFunction   = kwargs.get('drawFunc', None)
-        self.motionFunction = kwargs.get('motionFunc', None)
-        
+        self.mouseMoveFunction = kwargs.get('mouseMoveFunc', None)
+        self.clickFunction = kwargs.get('clickFunc', None)
+        self.mouseDownFunction = kwargs.get('mouseDownFunc', None)
+        self.mouseUpFunction = kwargs.get('mouseUpFunc', None)
+
+        bounds = kwargs.get('bounds', None)
+        if type(bounds) is tuple:
+            min_x = bounds[0]
+            min_y = bounds[1]
+            max_x = bounds[2]
+            max_y = bounds[3]
+
+            if max_x <= min_x:
+                raise ValueError('Max X cannot be less than or equal to min X')
+
+            if max_y <= min_y:
+                raise ValueError('Max Y cannot be less than or equal to min Y')
+                 
+            self.useBounds = True
+            self.bounds = (min_x, min_y, max_x, max_y)
+            self.scale_x = self.width // abs(max_x - min_x)
+            self.scale_y = self.width // abs(max_y - min_y)
+            bx = 0
+            if min_x >= 0:
+                bx = -(min_x * self.scale_x)
+            else:
+                bx = self.width - (max_x * self.scale_x)
+            by = 0
+            if min_y >= 0:
+                by = -(min_x * self.scale_y)
+            else:
+                by = (max_y * self.scale_y)
+            
+            self.bound_center = (bx, by)
 
         master.after(100, self.__setup())
         master.after(100, self.__animate())
         master.mainloop()
-        
+
+
+    def clearBounds(self):
+        self.useBounds = False
+
+    '''mouse related methods ---------------------------------------'''
+    def __get_mouse_positions(self, e):
+        if self.useBounds:
+            transformed = self.canvas.transform_coords([[e.x, e.y]])
+            self.mouse_x = transformed[0][0]
+            self.mouse_y = transformed[0][1]
+            self.mouse_x = self.tools.map(self.mouse_x,
+                                          0,
+                                          self.width,
+                                          self.bounds[0],
+                                          self.bounds[2])
+            self.mouse_y = self.tools.map(self.mouse_y,
+                                          0,
+                                          self.width,
+                                          self.bounds[3],
+                                          self.bounds[1])
+        else:
+            self.mouse_x = e.x - self.canvas.get_center_pos()[0]
+            self.mouse_y = e.y - self.canvas.get_center_pos()[1]
+
+        self.mouse_left = e.x
+        self.mouse_top = e.y
 
     def __motion_event(self, event):
-        self.mouse_x = event.x - self.canvas.get_center_pos()[0]
-        self.mouse_y = event.y - self.canvas.get_center_pos()[1]
-        self.mouse_left = event.x
-        self.mouse_top = event.y
-        if self.motionFunction:
-            self.motionFunction(self)
+        self.__get_mouse_positions(event)
+        if self.mouseMoveFunction:
+            self.mouseMoveFunction(self)
+
+    def __mouse_left_btn_click(self, event):
+        if self.clickFunction:
+            self.clickFunction(self, 'left')
+
+    def __mouse_middle_btn_click(self, event):
+        if self.clickFunction:
+            self.clickFunction(self, 'middle')
+
+    def __mouse_right_btn_click(self, event):
+        if self.clickFunction:
+            self.clickFunction(self, 'right')
+
+    def __left_mouse_btn_down(self, event):
+        self.__get_mouse_positions(event)
+        if self.mouseDownFunction:
+            self.mouseDownFunction(self, 'left')
+
+    def __right_mouse_btn_down(self, event):
+        self.__get_mouse_positions(event)
+        if self.mouseDownFunction:
+            self.mouseDownFunction(self, 'right')
+
+    def __left_mouse_btn_up(self, event):
+        if self.mouseUpFunction:
+            self.mouseUpFunction(self, 'left')
+
+    def __right_mouse_btn_up(self, event):
+        if self.mouseUpFunction:
+            self.mouseUpFunction(self, 'right')
+
+    '''-------------------------------------------------------------'''
         
     def __setup(self):
         if callable(self.setupFunction):
@@ -60,9 +179,7 @@ class EasyDraw(object):
         
     def __animate(self):
         if self.autoClear == True:
-            self.canvas.clear('all')
-            self.canvas.handle.create_rectangle(0, 0, self.width, self.height, fill = self.bg_color, outline = self.bg_color)
-        
+            self.canvas.clear('all')        
             self.canvas.clear_data()
 
         if callable(self.drawFunction):
@@ -70,7 +187,8 @@ class EasyDraw(object):
         else:
             raise Exception('Draw function is either undefined or not callable!')
 
-        if self.export_path != '': self.canvas.export_frame()
+        if self.export_path != '':
+            self.canvas.export_frame()
         self.canvas.handle.after(self.interval, self.__animate)
 
     def __on_closing(self):
